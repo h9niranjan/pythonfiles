e645e7de2b976c072b0222b6a3fd36cc7eb3d440
h9niranjan <161582821+h9niranjan@users.noreply.github.com>
2024-02-28 15:15:37 +0530

    Add files via upload

---
+++ datatype.py
@@ -0,0 +1,17 @@
+x=10
+y=11.5
+z=2+4j 
+a=""
+name='niranjan'
+city="banglore"
+multiline_coment='''this is multi line comment
+                                            in python'''
+
+#singleline_comment
+print(x)
+print(y)
+print(z)
+print(name)
+print(city[-1])
+print(a)
+
